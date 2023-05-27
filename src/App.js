724383c22534ca7441b8db27c60ee5ae4d457ed0
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-05-27 11:23:09 +0530

    create repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import { Knob } from "primereact/knob";
+import { InputText } from "primereact/inputtext";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [count, setCount] = useState(word);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    setCount(word.split(" "));
+  }, [count]);
+
+  useMemo(() => {
+    let setTime = 300;
+    setInterval(() => {
+      setTime -= 1;
+      if (setTime === 0) {
+        clearInterval();
+      }
+      setTimer(setTime);
+    }, 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" align="center">
+      <div className="cointainer">
+        <label htmlFor="" className="heading">
+          Test your typing skills
+        </label>
+        <Knob
+          valueColor={"Gray"}
+          rangeColor={"Green"}
+          textColor={"White"}
+          strokeWidth="5"
+          value={word ? timer : 299}
+          size={500}
+          min={0}
+          max={299}
+          readOnly
+        />
+        <h3 className="word-count">
+          Words Count: <span style={{ color: "cyan" }}>{count.length}</span>
+        </h3>
+        <InputText
+          className="input-box"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
